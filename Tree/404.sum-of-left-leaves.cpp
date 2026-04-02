844df42ac583df2200f677424682fe0c0deba40b
Pushpendu <myselfabhi2002@gmail.com>
2026-04-02 17:36:05 +0000

    Solution

---
+++ Tree/404.sum-of-left-leaves.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=404 lang=cpp
+ *
+ * [404] Sum of Left Leaves
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int sum = 0;
+
+        if(root->left && 
+           root->left->left == NULL && 
+           root->left->right == NULL)
+        {
+            sum += root->left->val;
+        }
+        sum += sumOfLeftLeaves(root->left);
+        sum += sumOfLeftLeaves(root->right);
+
+        return sum;
+    }
+};
+// @lc code=end
+
