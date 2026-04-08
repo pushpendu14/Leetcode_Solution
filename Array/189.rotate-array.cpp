7c8224d8fb3652b40ba3e93730bd6d9aff3ca723
Pushpendu <myselfabhi2002@gmail.com>
2026-04-08 16:48:34 +0000

    Added Solution

---
+++ Array/189.rotate-array.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=189 lang=cpp
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+
+        for(int i = 0; i < nums.size(); i++) {
+            cout << nums[i] << " ";
+        }
+    }
+};
+// @lc code=end
+
