37329738b34315858e0d3ee6d40a3befce9cb595
Pushpendu <myselfabhi2002@gmail.com>
2026-04-11 16:55:07 +0000

    Added Solution

---
+++ Array/283.move-zeroes.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=283 lang=cpp
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=-1;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return ;
+        
+        for(int i=j+1; i<nums.size(); i++){
+            if(nums[i] !=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+            
+        }
+    }
+};
+// @lc code=end
+
