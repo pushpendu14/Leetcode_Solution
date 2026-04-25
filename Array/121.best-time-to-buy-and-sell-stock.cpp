1401aca32cae1fa8dc7b4f8205d4bcc671c1abb5
Pushpendu <myselfabhi2002@gmail.com>
2026-04-25 21:03:52 +0000

    Solution

--- Array/121.best-time-to-buy-and-sell-stock.cpp
+++ Array/121.best-time-to-buy-and-sell-stock.cpp
@@ -8,7 +8,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int buy=INT_MAX, maxprofit=0;
+        for(int i=0; i<prices.size(); i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }else {
+                maxprofit = max(maxprofit, prices[i]-buy);
+            }
+        }
+
+        return maxprofit;
     }
 };
 // @lc code=end
--- Array/153.find-minimum-in-rotated-sorted-array.cpp
+++ Array/153.find-minimum-in-rotated-sorted-array.cpp
@@ -8,7 +8,19 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        
+        int n=nums.size();
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int mid=(left+right)/2;  
+            if(nums[mid]<nums[right]){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
+        }
+            
+        return nums[left];
     }
 };
 // @lc code=end
