30f428ee274e7c5244e3f417bf56291377e5530a
Pushpendu <myselfabhi2002@gmail.com>
2026-07-08 05:32:14 +0000

    Solution using Recursion

---
+++ Array/342.Power-Of-Four.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        // if(n<=0) return false;
+        // while(n!=1){
+        //     if(n%4 !=0) return false;
+        //     n=n/4;
+        // }
+        // return (n==1);
+
+	// Using Recursion
+
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(n%4 != 0) return false;
+        return isPowerOfFour(n/4);
+    }
+};
