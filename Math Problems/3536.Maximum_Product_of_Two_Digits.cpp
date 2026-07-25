df26b1febbed49b7a003904f6bb93a63748c555f
Pushpendu <myselfabhi2002@gmail.com>
2026-07-25 19:00:34 +0000

    Solution

---
+++ Math Problems/3536.Maximum_Product_of_Two_Digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int max1=-1;
+        int max2=-1;
+        while(n>0){
+            int d = n%10;
+            if(n%10 > max1){
+                max2=max1;
+                max1=d;
+            }else if(d > max2){
+                max2 = d;
+            } 
+            n /=10;
+        }
+        return max1*max2;
+    }
+};
