70c897e9b67e388616cb008b71ecf4f4cb3b8b97
Pushpendu <myselfabhi2002@gmail.com>
2026-07-30 05:59:08 +0000

    Solution

---
+++ Heap/215.Kth_Largest_Element_in_a_Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>q;
+      
+        for(int x:nums){
+            q.push(x);
+        }
+        while(k>1){
+            q.pop();
+            k--;
+        }
+        return q.top();
+    }
+};
